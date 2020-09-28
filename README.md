506b3e774dbd15f2e5438d6574cdf2f4f23cbc71
phindell <delphine.bugner@gmail.com>
2020-09-28 23:43:41 +0200

    Create React App - Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # SiteMaud
+## Consignes
+4 onglets
+- Accueil
+- Agenda
+- Biographie (fr + eng, avec version à télécharger et CV)
+- Médias (vidéos et audios)
+
+## Inspiration
+Interface plus claire (en terme de couleur), j'aime bien le défilement des infos :
+https://www.lesmusiciensdesaintjulien.fr/en/concerts/
+
+interface claire et simple. Photo en arrière plan.
+4 onglets.
+https://www.alexandrebaldo-art.com/media
---
+++ site-maud-front/src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './treble-clef.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h3>
+          Maud Haering
+        </h3>
+        <p>
+          Site en construction !
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
